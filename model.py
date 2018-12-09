1639eb3987ff1748ddd7dbc3b4262f5ffeefe055
Mostafa Yasin <mostafa.ys2014@gmail.com>
2018-12-09 23:15:15 +0100

    Completed The Simulation Table with completely random data.

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import random
 
 class Customer(object):
     def __init__(self, arrivalTime=0, serviceBegin=0):
+        self.id = 1
         self.interArrival = random.randint(1, 5)
         self.arrivalTime = arrivalTime
         self.serviceBegin = serviceBegin
