7d66a3ecd63b4cf721b23fb48a73b6248cd6a7c7
Mostafa Yasin <mostafa.ys2014@gmail.com>
2018-12-09 20:49:31 +0100

    Created the 'model.py' file which contains the schema of the customer.

---
+++ model.py
@@ -0,0 +1,9 @@
+import random
+
+class Customer(object):
+    def __init__(self, arrivalTime=0, serviceBegin=0):
+        self.interArrival = random.randint(1, 5)
+        self.arrivalTime = arrivalTime
+        self.serviceBegin = serviceBegin
+        self.serviceDuration = random.randint(1, 5)
+        self.serviceEnds = self.serviceBegin + self.serviceDuration
