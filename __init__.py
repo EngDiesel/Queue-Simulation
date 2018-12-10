89e2264e8096074b05beab6557c03c8dbc6bda0e
Mostafa Yasin <mostafa.ys2014@gmail.com>
2018-12-10 02:02:15 +0100

    Messy Code.

--- __init__.py
+++ __init__.py
@@ -10,56 +10,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    customers = []
-    # Manually Generate The first 'Customer' with completely random values.
-    customers.append(Customer())
-    customers[0].interArrival = None
-    customers[0].state = 0
+    return render_template('index.html')
 
-    # this loop generates 9 other 'Customers' and appends them to 'customers' list.
-    for i in range(1, 5):
-        print i
-        new = Customer()
-        _arrivalTime = customers[i-1].arrivalTime + new.interArrival
-        _serviceBegin = max(customers[i-1].serviceEnds, _arrivalTime)
-        _serviceEnd = _serviceBegin + new.serviceDuration
+customers = []
 
-        customers.append(new)
+# Manually Generate The first 'Customer' with completely random values.
+customers.append(Customer())
+customers[0].interArrival = None
 
-        # check for the customer state.
-    if new.arrivalTime < new.serviceBegin:
-        new.state = new.serviceBegin - new.arrivalTime
-    else:
-        new.state = 0
+# this loop generates 9 other 'Customers' and appends them to 'customers' list.
+for i in range(1, 10):
+    new = Customer()
 
-    for i in range(new.arrivalTime, new.serviceEnds):
-        new.points.append({'x': i, 'y': 1})
+    _arrivalTime = customers[i-1].arrivalTime + new.interArrival
+    _serviceBegin = max(customers[i-1].serviceEnds, _arrivalTime)
+    _serviceEnd = _serviceBegin + new.serviceDuration
+
+    new.arrivalTime = _arrivalTime
+    new.serviceBegin = _serviceBegin
+    new.serviceEnds = _serviceEnd
 
     customers.append(new)
 
-    # this loop generates 9 other 'Customers' and appends them to 'customers' list.
-    for i in range(1, 10):
-        new = Customer()
 
-        _arrivalTime = customers[i-1].arrivalTime + new.interArrival
-        _serviceBegin = max(customers[i-1].serviceEnds, _arrivalTime)
-        _serviceEnd = _serviceBegin + new.serviceDuration
-
-        new.arrivalTime = _arrivalTime
-        new.serviceBegin = _serviceBegin
-        new.serviceEnds = _serviceEnd
-
-        # ??
-        customers.append(new)
-
-
-    for customer in customers:
-        print "interArrival => " + str(customer.interArrival) + \
-         "\t| arrivalTime => " + str(customer.arrivalTime) +\
-         "\t| Service Begin => " + str(customer.serviceBegin) +\
-         "\t| Service Duration => " + str(customer.serviceDuration) +\
-         "\t| Service End => " + str(customer.serviceEnds)
-
+for customer in customers:
+    print "interArrival => " + str(customer.interArrival) + \
+     "\t| arrivalTime => " + str(customer.arrivalTime) +\
+     "\t| Service Begin => " + str(customer.serviceBegin) +\
+     "\t| Service Duration => " + str(customer.serviceDuration) +\
+     "\t| Service End => " + str(customer.serviceEnds)
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -2,8 +2,6 @@ import random
 
 class Customer(object):
     def __init__(self, arrivalTime=0, serviceBegin=0):
-        self.id = 1
-        self.points = []
         self.interArrival = random.randint(1, 5)
         self.state = 0
         self.arrivalTime = arrivalTime
